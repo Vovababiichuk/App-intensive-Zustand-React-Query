1363e60afe6613fef1b2e68a7febbff27140aa4e
VVVV BBBB <Volodymyr>
2024-04-12 19:37:55 +0300

    add Provider

---
+++ src/provider.tsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false
+		}
+	}
+})
+
+interface ProviderProps {
+	children: React.ReactNode;
+}
+
+export const Provider = ({ children }: ProviderProps) => {
+	return (
+		<QueryClientProvider client={queryClient}>
+			{children}
+			<ReactQueryDevtools initialIsOpen={false} />
+		</QueryClientProvider>
+	);
+};
